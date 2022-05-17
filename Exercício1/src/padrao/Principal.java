a258a5f2176229cf7572800c23b8d7ab360955ee
ucasvieira <lucas.souza1012@etec.sp.gov.br>
2022-05-17 09:00:54 -0300

    Exercício 1

---
+++ Exercício1/src/padrao/Principal.java
@@ -0,0 +1,12 @@
+package padrao;
+
+import javax.swing.JFrame;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Interface app = new Interface();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
